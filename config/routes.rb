8c3bc3c78b5ddc5d707285119d281a4def19bb64
EC2 Default User <ec2-user@ip-172-31-18-106.ap-northeast-1.compute.internal>
2022-07-31 05:48:23 +0000

    [Add]bookのcreate機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get 'books/new'
 
-  post 'books' => 'books#create'
-
-  get 'books' => 'books#index'
-  get 'books/show'
   get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  get '/top' => 'homes#top'
+
+ post 'books' => 'books#create'
+
+ get 'books' => 'books#index'
+
+ get 'books/:id' => 'books#show', as: 'book'
+
 end
