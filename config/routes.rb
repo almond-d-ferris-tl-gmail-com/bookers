584fd26d288e989cfb3c0d32020bb07524d6a4e7
EC2 Default User <ec2-user@ip-172-31-18-106.ap-northeast-1.compute.internal>
2022-07-31 11:16:56 +0000

    [Update]bookのshowページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  get 'books/new'
 
-  get 'books/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- get '/top' => 'homes#top'
+  get '/' => 'homes#top'
+  resources :books
 
- post 'books' => 'books#create'
+  # get 'books/new'
 
- get 'books' => 'books#index'
+  # post 'books' => 'books#create'
 
- get 'books/:id' => 'books#show', as: 'book'
+  # get 'books' => 'books#index'
 
+  # get 'books/:id' => 'books#show', as: 'book'
+
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+
+  # patch 'books/:id' => 'books#update', as: 'update_book'
+
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
